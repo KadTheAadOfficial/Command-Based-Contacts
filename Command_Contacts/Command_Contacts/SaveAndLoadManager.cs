4f99b70591fc6d544978861a29ed0cbef9d1f2e3
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2020-08-04 15:35:07 +0400

    Full Project

---
+++ Command_Contacts/Command_Contacts/SaveAndLoadManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Command_Contacts
+{
+    class SaveAndLoadManager
+    {
+        public static void Save (string email, string phone, string fullName)
+        {
+            string path = "C:/Users/wissamadib/Documents/" + fullName + ".contacts";
+            string data = fullName + ":" + "\nEmail: " + email + "\nPhone: " + phone;
+            File.WriteAllText(path, data);
+        }
+        public static string Load (string fullName)
+        {
+            string path = "C:/Users/wissamadib/Documents/" + fullName + ".contacts";
+            string data = File.ReadAllText(path);
+            return data;
+        }
+        public static void Delete (string fullName)
+        {
+            string path = "C:/Users/wissamadib/Documents/" + fullName + ".contacts";
+            Console.Write("Are you sure? (Y/n): ");
+            if (Console.ReadLine() == "Y")
+            {
+                File.Delete(path);
+                Console.WriteLine("File Deleted");
+                return;
+
+            } else 
+            {
+                return;
+            }
+        }
+    }
+}
