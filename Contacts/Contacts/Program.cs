ff3bfccdf0a773320123d15cfeed497abac17696
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2020-08-05 12:14:31 +0400

    Updated Version

---
+++ Contacts/Contacts/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Contacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Contacts";
+            if (args.Length == 0)
+            {
+                Program();
+            } else
+            {
+                SaveAndLoadManager.Input(args[0]);
+                Program();
+            }
+            void Program()
+            {
+                string function;
+                function = Console.ReadLine();
+
+                if (function == "new")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Email: ");
+                    string email = Console.ReadLine();
+
+                    Console.Write("Phone: ");
+                    string phone = Console.ReadLine();
+
+                    SaveAndLoadManager.Save(email, phone, name);
+
+                    Console.WriteLine("Saved!");
+
+                    Program();
+                } else if (function == "load")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine(SaveAndLoadManager.Load(name));
+                    Program();
+                } else if (function == "delete")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    SaveAndLoadManager.Delete(name);
+                    Program();
+                }
+                Program();
+            }
+        }
+    }
+}
---
+++ Contacts/Contacts/SaveAndLoadManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Contacts
+{
+    class SaveAndLoadManager
+    {
+        public static void Save (string email, string phone, string name)
+        {
+            string path = "C:/Users/wissamadib/Documents/Contacts/" + name + ".clcontacts";
+            string data = name + ":" + "\nEmail: " + email + "\nPhone: " + phone;
+            File.WriteAllText(path, data);
+            return;
+        }
+        public static string Load (string name)
+        {
+            string path = "C:/Users/wissamadib/Documents/Contacts/" + name + ".clcontacts";
+            string data = File.ReadAllText(path);
+            return data;
+        }
+        public static void Delete (string name)
+        {
+            string path = "C:/Users/wissamadib/Documents/Contacts/" + name + ".clcontacts";
+            Console.Write("Are you sure? (Y/n): ");
+            if (Console.ReadLine() == "Y")
+            {
+                File.Delete(path);
+                Console.WriteLine("File Deleted");
+                return;
+            } else if (Console.ReadLine() == "n")
+            {
+                return;
+            }
+        }
+        public static void Input (string path)
+        {
+            string data = File.ReadAllText(path);
+            Console.WriteLine(data);
+            return;
+        }
+    }
+}
